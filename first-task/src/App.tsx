bb84b15580cf8068f994f914dd1d84956027756a
Anibal <hello@anibalgatica.com>
2022-09-09 20:02:30 +0100

    Part 3 face animated

--- first-task/src/App.tsx
+++ first-task/src/App.tsx
@@ -1,7 +1,10 @@
+import SmileyFace from "views/SmileyFace";
+
 export default function App() {
   return (
-    <div className="flex justify-center items-center pb-32 w-full h-screen bg-slate-200">
+    <div className="flex flex-col justify-center items-center pb-32 w-screen h-screen bg-slate-200">
       <h1 className="text-4xl font-semibold">Hello simple world</h1>
+      <SmileyFace />
     </div>
   );
 }
