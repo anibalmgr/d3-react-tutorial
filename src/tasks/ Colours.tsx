67f0b2a1f9418bed003f16077367beb53c01ab79
Anibal <hello@anibalgatica.com>
2022-09-24 09:51:01 +0100

    Circle reacts to mouse

--- src/tasks/ Colours.tsx
+++ src/tasks/ Colours.tsx
@@ -1,5 +1,8 @@
 import { arc, csv, DefaultArcObject, DSVRowString } from "d3";
-import { useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
+
+const width = 550;
+const height = 500;
 
 const csvUrl =
   "https://gist.githubusercontent.com/anibalmgr/11ae839ceadc301edf073bfe048b09cd/raw/890dd982e3c44e6c5843bf330abd0aa286fdebed/css-colours.csv";
@@ -14,6 +17,13 @@ export default function Colours() {
   const [data, setData] = useState<
     Array<Colour> | Array<DSVRowString<string>> | null
   >(null);
+  const [center, setCenter] = useState({ x: width / 2, y: height / 2 });
+
+  const handleMouse = (e: MouseEvent<HTMLDivElement>) => {
+    const positionX = e.clientX;
+    const positionY = e.clientY;
+    setCenter({ x: positionX, y: positionY - 145 });
+  };
 
   useEffect(() => {
     csv(csvUrl).then(setData);
@@ -22,27 +32,24 @@ export default function Colours() {
   if (!data) {
     return <pre>Loading...</pre>;
   }
-  const width = 550;
-  const height = 200;
-  const center = { x: width / 2, y: height / 2 };
 
   const pieArc: DefaultArcObject | any = arc()
     .innerRadius(0)
     .outerRadius(width);
 
-  console.log(pieArc);
-
   return (
-    <div className="flex flex-wrap w-full">
-      <svg width={width} height={height}>
-        <g transform={`translate${center.x},${center.y}`}>
+    <div className="flex flex-wrap w-full h-full" onMouseMove={handleMouse}>
+      <svg className="w-full h-full">
+        <g transform={`translate(${center.x},${center.y})`}>
           {data.map((d, i) => (
             <path
               key={`${d.Keyword}${i}`}
               fill={d["RGB hex value"]}
               d={pieArc({
-                startAngle: (i / data.length) * 2 * Math.PI,
-                endAngle: ((i + 1) / data.length) * 2 * Math.PI,
+                startAngle:
+                  (i / data.length) * (1.8 + center.x / 4000) * Math.PI,
+                endAngle:
+                  ((i + 1) / data.length) * (1.8 + center.y / 4000) * Math.PI,
               })}
             />
           ))}
